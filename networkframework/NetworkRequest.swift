a1643b70bdd746ccff8cc6c7e9bd518881390110
Avaneesh Kumar <awasthi.avaneesh@gmail.com>
2025-06-26 17:00:45 +0530

    Make RequestProtocol public and add Foundation import for SPM compatibility

--- networkframework/NetworkRequest.swift
+++ networkframework/NetworkRequest.swift
@@ -3,30 +3,31 @@
 //  networkframework
 //
 
+import Foundation
 import Combine
 
-protocol RequestProtocol {
+public protocol RequestProtocol {
     func sendRequest<T: Decodable>(endpoint: EndPoint) async throws -> T
     func sendRequest<T: Decodable>(endpoint: EndPoint, resultHandler: @escaping (Result<T, APIServiceError>) -> Void)
     func sendRequest<T: Decodable>(endpoint: EndPoint, type: T.Type) -> AnyPublisher<T, APIServiceError>
 }
 
-extension RequestProtocol {
-        func createRequest(endPoint: EndPoint) -> URLRequest? {
-            var urlComponents = URLComponents()
-            urlComponents.scheme = endPoint.scheme
-            urlComponents.host = endPoint.host
-            urlComponents.path = endPoint.path
-            guard let url = urlComponents.url else {
-                return nil
-            }
-            let encoder = JSONEncoder()
-            var request = URLRequest(url: url)
-            request.httpMethod = endPoint.method.rawValue
-            request.allHTTPHeaderFields = endPoint.header
-            if let body = endPoint.body {
-                request.httpBody = try? encoder.encode(body)
-            }
-            return request
+public extension RequestProtocol {
+    func createRequest(endPoint: EndPoint) -> URLRequest? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endPoint.scheme
+        urlComponents.host = endPoint.host
+        urlComponents.path = endPoint.path
+        guard let url = urlComponents.url else {
+            return nil
         }
+        let encoder = JSONEncoder()
+        var request = URLRequest(url: url)
+        request.httpMethod = endPoint.method.rawValue
+        request.allHTTPHeaderFields = endPoint.header
+        if let body = endPoint.body {
+            request.httpBody = try? encoder.encode(body)
+        }
+        return request
     }
+}
